1c28cdda349204b382742d73d0778c6453e01b79
Raphael Loyola <raphael.braccialli@gmail.com>
2019-09-10 23:42:11 -0300

    escreve a cada hora. Integrar com receptor.

--- file_saver.c
+++ file_saver.c
@@ -5,16 +5,48 @@
 int main()
 {
 
-time_t currentTime;
-struct tm *myTime;
-int i = 0;
+struct tm currentTime;
+struct tm oldTime;
+time_t ltime;
+int k = 0;
+char file_name[50];
+//FILE *file_hour;
 
-while(i < 1){
-    time(&currentTime);
-    *myTime = localtime(&currentTime);
-    print("%i/%i/%i\n", myTime->tm_mon+1, myTime->tm_mday, myTime->tm_year+1900);
-    i++;
+
+
+
+
+//file = fopen("file1.txt", "a");
+
+while(k < 1){
+	ltime=time(&ltime);
+    localtime_r(&ltime, &currentTime);
+	localtime_r(&ltime, &oldTime);
+
+
+
+    while(currentTime.tm_min == oldTime.tm_min){ // trocar pra hora
+    	ltime=time(&ltime);
+    	localtime_r(&ltime, &currentTime);
+
+    	// rotina de salvar dados
+
+    }
+
+    sprintf(file_name, "%i_%i_%i_%i\n", currentTime.tm_year+1900, currentTime.tm_mon+1, currentTime.tm_mday, currentTime.tm_hour);
+    printf("%s", file_name);
+    // file_hour = file = fopen(file_name, "a");
+    // escreve arquivo, salva estimativa
+    // while(i<size(msg_buffer)){
+    // fputs("This is testing for fputs...\n", file_hour);
+	// }
+    
+    // fclose(file_name)
+
+    k++;
     
 }
+
+//fclose(file);
 
 }
